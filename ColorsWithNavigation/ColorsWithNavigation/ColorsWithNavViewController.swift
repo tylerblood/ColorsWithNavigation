44d95a6076b45b5a35b8cf674b6b30ef1e57ca6f
Tyler Blood <twbcdc@mail.missouri.edu>
2019-04-05 21:33:43 -0500

    Create functionality of colors in table view

---
+++ ColorsWithNavigation/ColorsWithNavigation/ColorsWithNavViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ColorsWithNavViewController.swift
+//  ColorsWithNavigation
+//
+//  Created by Tyler Blood on 4/5/19.
+//  Copyright © 2019 Tyler Blood. All rights reserved.
+//
+
+import UIKit
+
+class ColorsWithNavViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var colors = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        
+        cell.textLabel?.text = colors[indexPath.row]
+        
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
