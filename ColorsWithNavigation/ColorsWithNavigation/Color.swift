9e93e33b306300db969a1cfecb473782f4e551dd
Tyler Blood <twbcdc@mail.missouri.edu>
2019-04-05 21:44:07 -0500

    Added color structure and background colors to table

---
+++ ColorsWithNavigation/ColorsWithNavigation/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  ColorsWithNavigation
+//
+//  Created by Tyler Blood on 4/5/19.
+//  Copyright © 2019 Tyler Blood. All rights reserved.
+//
+
+import UIKit
+
+struct Color{
+    let name: String
+    let uiColor: UIColor
+}
